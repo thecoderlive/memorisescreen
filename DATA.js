bcb99e016fb611c82e738b6e4713accedda82224
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-23 15:45:16 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,27 @@
+export const item = {
+    "id": "7c9de843-8b4a-4bfe-816e-24bbd35e67d4",
+    "memorise_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/328.png",
+    "refresh_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png",
+    "option_list": [
+        {
+            "id": "ffc76c5d-4185-4706-a309-9ed9f1ea8a80",
+            "option": "purpose"
+        },
+        {
+            "id": "a7675ffe-c786-4b3f-be53-c7e25c90e687",
+            "option": "management"
+        },
+        {
+            "id": "bb8bc490-0bd0-40ff-b347-c7ac8439cc03",
+            "option": "behavior"
+        },
+        {
+            "id": "c36d42cc-c979-407d-b60b-ee8eb83af844",
+            "option": "baby"
+        },
+        {
+            "id": "c3222227-36b4-4298-840e-e485e771e622",
+            "option": "anything"
+        }
+    ]
+}
